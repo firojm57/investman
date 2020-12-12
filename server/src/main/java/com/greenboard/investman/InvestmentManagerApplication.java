2e5471b1616495422ebea5a3861b8789db02ae9a
Firoj Mujawar <firojm57@gmail.com>
2020-12-12 23:55:34 +0530

    Initial spring boot setup.

---
+++ server/src/main/java/com/greenboard/investman/InvestmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.greenboard.investman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentManagerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/greenboard/investman/InvestmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenboard.investman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
